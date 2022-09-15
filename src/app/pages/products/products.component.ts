46051c77afd8aa6fe065df0c96d1a150be62c8a6
MuniBarbieri <edmundobarbieri@gmail.com>
2022-09-15 11:25:21 -0300

    *ProductsComponent: Create products property,  store  productList *ProductsModule: Import  MaterialModule *MaterialModule: Import MatCardModule *PrductsTemplate: Add mat-card tag

--- src/app/pages/products/products.component.ts
+++ src/app/pages/products/products.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './interfaces/product.interface';
 import { ProductsService } from './services/products.service';
 import { Component, OnInit } from '@angular/core';
 import { tap } from 'rxjs';
@@ -8,12 +9,13 @@ import { tap } from 'rxjs';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [];
   constructor(private productSvc: ProductsService) {}
 
   ngOnInit(): void {
     this.productSvc
       .getProducts()
-      .pipe(tap((res) => console.log(res)))
+      .pipe(tap((productList: Product[]) => (this.products = productList)))
       .subscribe();
   }
 }
