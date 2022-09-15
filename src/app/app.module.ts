031fabf2f797284aaccfb5ee664b33e9bf4da57a
MuniBarbieri <edmundobarbieri@gmail.com>
2022-09-15 10:43:21 -0300

    *ProductsService: create getProducts method *ProductsComponent: Inject ProductsService, Call getProducts *AppModule: Import HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -15,6 +16,7 @@ import { MaterialModule } from './material.module';
     MaterialModule,
     FormsModule,
     BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
